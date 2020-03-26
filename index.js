8732f271f126b7a42fc2c1b70788b4f4c473db15
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-26 13:17:47 -0600

    Add basic observable support

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 function nodeCreator(nodeName) {
   return function (attributes, ...children) {
       let el = document.createElement(nodeName);
-      if (typeof attributes !== 'object' && typeof attributes !== 'string') {
-        throw Error("Expected first parameter to be type object or string, got " + typeof attributes);
-      }
-      if (attributes instanceof Element) {
-        el.appendChild(attributes);
-      }
-      else if (typeof attributes === 'string') {
-        el.appendChild(document.createTextNode(attributes));
-      }
-      else {
+
+      let allChildren;
+      if (!(attributes instanceof Element || typeof attributes.subscribe === 'function' || typeof attributes === 'string')) {
+        //It's an attributes map
         for (let attr of Object.keys(attributes)) {
-          if (typeof attributes[attr] === "function") {
-            el[attr] = attributes[attr]
+          let value = attributes[attr];
+          if (typeof value === "function") {
+            el[attr] = value;
           }
+          else if (typeof value.subscribe === 'function') {
+            value.subscribe(v => el.setAttribute(attr, v));
+          } 
           else {
             el.setAttribute(attr, attributes[attr]);
           }
         }
+        allChildren = children;
       }
-      for (let child of children) {
+      else {
+        allChildren = [attributes, ...children];
+      }
+
+      for (let child of allChildren) {
         if (typeof child === "string") {
           el.appendChild(document.createTextNode(child))
         }
-        else {
+        else if (typeof child.subscribe === 'function') {
+          let currentChild = document.createTextNode("");
+          el.appendChild(currentChild);
+          child.subscribe(newVal => {
+            if (newVal instanceof Element) {
+              el.replaceChild(newVal, currentChild);
+              currentChild = newVal;
+            }
+            else {
+              let tmp = document.createTextNode(`${newVal}`);
+              el.replaceChild(tmp, currentChild);
+              currentChild = tmp;
+            }
+          });
+        }
+        else if (child instanceof Element) {
           el.appendChild(child);
+        }
+        else {
+          throw Error("Expected children to each be Element, string, or Observable");
         }
       }
       return el;
